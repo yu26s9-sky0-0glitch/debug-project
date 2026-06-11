64c9e848bf0b281081dce6c0958f5713b810c3a8
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2026-06-11 16:12:14 -0400

    bug 4 done

--- src/main/java/nl/pluralsight/stagepass/service/BookingService.java
+++ src/main/java/nl/pluralsight/stagepass/service/BookingService.java
@@ -42,7 +42,8 @@ public class BookingService {
                 .orElseThrow(() -> new RuntimeException("Concert not found"));
 
          //Compute total price
-        booking.setTotalPrice(BigDecimal.ZERO);
+
+        booking.setTotalPrice(concert.getTicketPrice().multiply(new BigDecimal(booking.getNumberOfTickets())));
 
        //  Set booking date and concert reference
         booking.setBookingDate(LocalDate.now());
